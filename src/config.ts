6d4e186ad824f8022a9802a778c331b093e39c73
PaulHaze <dev@artemist.com.au>
2024-07-02 17:28:49 +1000

    update site info

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-//! CHANGETHIS
-export const SITE_TITLE = 'Astro Playground';
-export const SITE_DESCRIPTION =
-	'A place for experiments, code practise and boundary pushing';
+export const SITE_TITLE = 'GSAP Playground';
+export const SITE_DESCRIPTION = 'A site to experiment with GSAP';
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
